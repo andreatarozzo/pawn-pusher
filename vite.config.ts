324cbd039c5c24e47c9454617b3bbf9472ec7d54
Andrea Tarozzo <tarozzo.andrea@gmail.com>
2024-04-29 12:43:32 +0100

    feat: added PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,46 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['logo192.png', 'logo512.png'],
+      manifest: {
+        short_name: 'PawnsBooper',
+        name: 'PawnsBooper',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo192.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#FFFFFF',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@/': new URL('./src/', import.meta.url).pathname,
