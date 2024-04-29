49879d5e788155e008c1f2a46d49866d3f0cb65f
Andrea Tarozzo <tarozzo.andrea@gmail.com>
2024-04-29 23:02:11 +0100

    tests: added tests for PawnCoordinatesSummary component

--- src/components/PawnCoordinatesSummary/PawnCoordinatesSummary.test.tsx
+++ src/components/PawnCoordinatesSummary/PawnCoordinatesSummary.test.tsx
@@ -1,7 +1,46 @@
-import { describe, it, expect } from 'vitest';
+import { BoardSize } from '@/types';
+import { GameBoard, GameState } from '@/utils';
+import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { PawnCoordinatesSummary } from './PawnCoordinatesSummary';
 
 describe('DebugPanel', () => {
-  it('', () => {
-    expect(true);
+  let gameState: GameState;
+
+  beforeEach(() => {
+    gameState = new GameState(new GameBoard(BoardSize.Rows, BoardSize.Cols));
+  });
+
+  it('Should render just the pawn images when no pawn coordinates are present in the game state', () => {
+    render(<PawnCoordinatesSummary gameState={gameState} />);
+
+    expect(screen.getByText('Pawn Summary'));
+
+    const images = screen.queryAllByRole('img');
+    expect(images[0].getAttribute('alt')).toBe('blue kitten');
+    expect(images[1].getAttribute('alt')).toBe('blue cat');
+    expect(images[2].getAttribute('alt')).toBe('orange kitten');
+    expect(images[3].getAttribute('alt')).toBe('orange cat');
+  });
+
+  it('Should render just the pawn images when no pawn coordinates are present in the game state', () => {
+    gameState.pawnsCoordinates[1].Kitten.push([0, 0]);
+    gameState.pawnsCoordinates[1].Cat.push([0, 1]);
+    gameState.pawnsCoordinates[2].Kitten.push([5, 0]);
+    gameState.pawnsCoordinates[2].Cat.push([5, 1]);
+    render(<PawnCoordinatesSummary gameState={gameState} />);
+
+    expect(
+      screen.getByTestId('player-1-kitten-pawn-coordinates-summary').children.item(1)?.textContent,
+    ).toBe('[0,0]');
+    expect(
+      screen.getByTestId('player-1-cat-pawn-coordinates-summary').children.item(1)?.textContent,
+    ).toBe('[0,1]');
+    expect(
+      screen.getByTestId('player-2-kitten-pawn-coordinates-summary').children.item(1)?.textContent,
+    ).toBe('[5,0]');
+    expect(
+      screen.getByTestId('player-2-cat-pawn-coordinates-summary').children.item(1)?.textContent,
+    ).toBe('[5,1]');
   });
 });
--- src/components/PawnCoordinatesSummary/PawnCoordinatesSummary.tsx
+++ src/components/PawnCoordinatesSummary/PawnCoordinatesSummary.tsx
@@ -16,23 +16,27 @@ export const PawnCoordinatesSummary: FC<PawnCoordinatesSummaryProps> = ({
       <div>
         <strong>Pawn Summary</strong>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center" data-testid="player-1-kitten-pawn-coordinates-summary">
         <img alt={`blue kitten`} className="h-7 w-7 mt-2 cursor-pointer" src="/kitten-blue.png" />
         {gameState.pawnsCoordinates[Player.PlayerOne][PawnType.Kitten].map(
           (coordinate: Coordinate) => (
-            <span className="not-first:ml-2">{JSON.stringify(coordinate)}</span>
+            <span aria-label="player 1 kitten pawns coordinates" className="not-first:ml-2">
+              {JSON.stringify(coordinate)}
+            </span>
           ),
         )}
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center" data-testid="player-1-cat-pawn-coordinates-summary">
         <img alt={`blue cat`} className="h-7 w-7 mt-2 cursor-pointer" src="/cat-blue.png" />
         {gameState.pawnsCoordinates[Player.PlayerOne][PawnType.Cat].map(
           (coordinate: Coordinate) => (
-            <span className="not-first:ml-2">{JSON.stringify(coordinate)}</span>
+            <span aria-label="player 1 cat pawns coordinates" className="not-first:ml-2">
+              {JSON.stringify(coordinate)}
+            </span>
           ),
         )}
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center" data-testid="player-2-kitten-pawn-coordinates-summary">
         <img
           alt={`orange kitten`}
           className="h-7 w-7 mt-2 cursor-pointer"
@@ -40,15 +44,19 @@ export const PawnCoordinatesSummary: FC<PawnCoordinatesSummaryProps> = ({
         />
         {gameState.pawnsCoordinates[Player.PlayerTwo][PawnType.Kitten].map(
           (coordinate: Coordinate) => (
-            <span className="not-first:ml-2">{JSON.stringify(coordinate)}</span>
+            <span aria-label="player 2 kitten pawns coordinates" className="not-first:ml-2">
+              {JSON.stringify(coordinate)}
+            </span>
           ),
         )}
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center" data-testid="player-2-cat-pawn-coordinates-summary">
         <img alt={`orange cat`} className="h-7 w-7 mt-2 cursor-pointer" src="/cat-orange.png" />
         {gameState.pawnsCoordinates[Player.PlayerTwo][PawnType.Cat].map(
           (coordinate: Coordinate) => (
-            <span className="not-first:ml-2">{JSON.stringify(coordinate)}</span>
+            <span aria-label="player 2 cat pawns coordinates" className="not-first:ml-2">
+              {JSON.stringify(coordinate)}
+            </span>
           ),
         )}
       </div>
